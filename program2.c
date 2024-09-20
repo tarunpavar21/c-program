03e66df1e9fbcb5fe73ca255b864ce7a04b9a299
tarunpavar21 <tarunpavar11@gmail.com>
2024-09-20 11:42:45 +0530

    TO print integer vaue in program

---
+++ program2.c
@@ -0,0 +1,15 @@
+// TO print integer vaue in program 
+#include<stdio.h>
+
+int main()
+{
+    int a=5; 
+
+    printf("the value of iteger A = %d",a);
+    return 0;
+}
+
+// note
+// int a=5; creating the variable which name is a and it's value is 5
+// you can change the variable name as well as the value of it 
+// %d used to represent integer values as signed decimals.
