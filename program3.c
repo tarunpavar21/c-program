8630acf524279ee3536b67dd36bb2af2e88119eb
tarunpavar21 <tarunpavar11@gmail.com>
2024-09-20 11:47:03 +0530

    Reading Integer values in C

---
+++ program3.c
@@ -0,0 +1,21 @@
+//Reading Integer values in C
+// C program to take an integer
+// as input and print it
+
+
+#include <stdio.h>
+
+int main()
+{
+	// Declare the variables
+	int number;
+
+	// Input the integer
+	printf("Enter the integer: ");
+	scanf("%d", &number);
+
+	// Display the integer
+	printf("Entered integer is: %d", number);
+
+	return 0;
+}
