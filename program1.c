27fa57dd9307f6088284fee4231e08627e8856c7
tarunpavar21 <tarunpavar11@gmail.com>
2024-09-20 11:40:31 +0530

    display hello world

---
+++ program1.c
@@ -0,0 +1,18 @@
+// Simple C program to display "Hello World"
+
+// Header file for input output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+
+    // writing print statement to print hello world
+    printf("Hello World");
+
+    return 0;
+}
+// #include <stdio.h> – This line includes the standard input-output library in the program.
+// int main() – The main function where the execution of the program begins.
+// printf(“Hello, World!\n”); – This function call prints “Hello, World!” followed by a new line.
+// return 0; -This statement indicates that the program ended successfully.
+
