a57142a390388fefd3b1bf0abdf77f73daab5b12
tarunpavar21 <tarunpavar11@gmail.com>
2024-09-22 22:53:14 +0530

    addition of two number

---
+++ program4.c
@@ -0,0 +1,34 @@
+//C Program to Add Two Integer
+// here we define 3 variable two variable for define value and third for storing the answer of addition 
+// and simple we just adding two number 
+
+#include <stdio.h>
+
+int main() {
+    int a, b, sum = 0;
+
+    // Read two numbers from the user
+    printf("Enter integer A: ");
+    scanf("%d", &a);  
+    printf("Enter integer B: ");
+    scanf("%d", &b);  
+    // Calculate the addition of a and b using the '+' operator
+    sum = a + b;
+
+    // Print the result
+    printf("Sum: %d", sum);
+
+    return 0;
+}
+
+
+
+// Types of Operators in C
+// C language provides a wide range of operators that can be classified into 6 types based on their functionality:
+
+// Arithmetic Operators
+// Relational Operators
+// Logical Operators
+// Bitwise Operators
+// Assignment Operators
+// Other Operators
